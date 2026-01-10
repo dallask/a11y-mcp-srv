713b028f4a8e3826cfdbb8644aaf09acf6cf50cf
Shekh <shekh@atlan.com>
2026-01-10 16:30:53 +0530

    chore: Update bin script comment with new package name

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Wrapper script for npx execution
-// This file is executed when running: npx @ali0113/accessibility-audit-mcp-server
+// This file is executed when running: npx @ali0113/accessibility-mcp-server
 import('../dist/server.js').catch((error) => {
   console.error('Error starting server:', error);
   process.exit(1);
